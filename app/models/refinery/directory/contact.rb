d5f1fc11dd5e975c99a6510bee7d8a8ecf02cccc
Agustín Leñero <agush@roca9.com>
2012-06-27 11:54:33 -0500

    Added current user as author and made attr_accessible

--- app/models/refinery/directory/contact.rb
+++ app/models/refinery/directory/contact.rb
@@ -10,12 +10,12 @@ module Refinery
 
     attr_accessible :name, :position, :city, :state, :zip_code, :address
     attr_accessible :logo_id, :phone, :email, :contact, :info, :abstract, :web, :facebook
-    attr_accessible :twitter, :tag_list, :category_ids
+    attr_accessible :twitter, :tag_list, :category_ids, :user_id, :author
 
     acts_as_indexed :fields => [:name]
 
     validates :name, :presence => true, :uniqueness => true
-    
+
     belongs_to :logo, :class_name => '::Refinery::Image'
     belongs_to :author, :class_name => '::Refinery::User', :foreign_key => :user_id, :readonly => true
     has_and_belongs_to_many :categories
